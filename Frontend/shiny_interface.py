d663c42781cc80faf437d82fe794600dc38abc69
bedoosd <teck_kenneth@hotmail.com>
2025-05-09 15:33:22 +0200

    available years, countries logic chart select logic

--- Frontend/shiny_interface.py
+++ Frontend/shiny_interface.py
@@ -2,24 +2,22 @@ from shiny import App, ui, render
 from shinywidgets import output_widget, render_widget
 import pandas as pd
 import plotly.graph_objects as go
+#from ??? import Tournament
 
-# Dummy data
-df = pd.DataFrame({
-    "month": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    "births": [1200, 1100, 1300, 1250, 1400, 1350, 1500, 1480, 1420, 1380, 1300, 1250]})
-tournaments = ["World Championship", "European Championship"]
-years = ["2024", "2020", "2016"]
-countries = ["Belgium", "France", "Germany"]
+wc = Tournament("World Championship")
+ec = Tournament("European Championship")
+
+tournaments = {"World Championship": wc, "European Championship": ec}
+#makes x an instance of Tournament as in x = tournaments[input.tournament_selection()]
 
 # adjusted size to have both championships on 1 line: 300px or 2lines 200px
 custom_style = ui.tags.style(
-    """aside.sidebar {width: 200px !important; min-width: 200px !important;}"""
-    )
+    """aside.sidebar {width: 200px !important; min-width: 200px !important;}""")
 
 # UI layout
 app_ui = ui.page_sidebar(
     ui.sidebar(
-        ui.input_radio_buttons("tournament_selection", "Select a tournament:", tournaments),
+        ui.input_radio_buttons("tournament_selection", "Select a tournament:", tournaments ),
         ui.input_select("available_years_selection"),
         ui.input_radio_buttons("available_countries_selection"),
         bg="#f8f8f8"
@@ -34,14 +32,28 @@ def server(inputs, outputs, session):
     @outputs
     @render.ui
     def available_years_selection():
-        #logic to get years from tournament
-        pass
+        tournament = tournaments[inputs["tournament_selection"]()]
+        available_years = tournament.get_available_years()
+        return ui.input_select("available_years_selection", "Select year:", available_years)
+
     def available_countries_selection():
-        #logic to get countries from tournament and year
-        pass
+        tournament = tournaments[inputs["tournament_selection"]()]
+        available_countries = tournament.get_available_countries(inputs["available_years_selection"]())
+        return ui.input_radio_buttons("available_countries_selection", "Select countries:", available_countries)
+
     def birth_chart():
-        #logic to select monthly graph, yearly graph or empty graph
-        pass
+        tournament = tournaments[inputs["tournament_selection"]()]
+        has_monthly_data = tournament.get_has_montly_data()
+        has_yearly_data = tournament.get_has_yearly_data()
+
+        if has_monthly_data:
+            return monthly_chart()
+
+        elif has_yearly_data:
+            return yearly_chart()
+
+        else: return no_data_chart()
+
 
     @render_widget
     def monthly_chart():
@@ -67,6 +79,7 @@ def server(inputs, outputs, session):
             yaxis_title="Number of Births"
         )
         return fig
+
     @render_widget
     def yearly_chart():
         #design yearly graph when no monthly available
@@ -76,6 +89,7 @@ def server(inputs, outputs, session):
     def no_data_chart():
         #design an empty chart with message
         pass
+
 app = App(app_ui, server)
 
 if __name__ == "__main__":

a98adc7a36ec8a4ca0cfa338f137f387c7289c9a
bedoosd <teck_kenneth@hotmail.com>
2025-05-10 14:39:33 +0200

    Made 1 dynamic graph for both yearly and monthly. Added dummy data for testing Adjustments to make it work

--- Frontend/shiny_interface.py
+++ Frontend/shiny_interface.py
@@ -2,27 +2,71 @@ from shiny import App, ui, render
 from shinywidgets import output_widget, render_widget
 import pandas as pd
 import plotly.graph_objects as go
-#from ??? import Tournament
 
-wc = Tournament("World Championship")
-ec = Tournament("European Championship")
+#generated some dummy data to be able to test the app without backend
+class MockTournament:
+    def __init__(self, name):
+        self.name = name
+
+    def get_available_years(self):
+        if self.name == "European Championship":
+            return ["2021", "2022", "2023"]
+        else: return ["2017","2018", "2019", "2020"]
+
+    def get_available_countries(self, year):
+        if self.name == "European Championship":
+            if year == "2021": return ["Belgium", "Bulgaria", "Croatia"]
+            if year == "2022": return ["Czech Republic", "Denmark", "Finland"]
+            if year == "2023": return ["France", "Belgium", "Italy"]
+        else:
+            if year == "2017": return ["Japan", "Germany", "France"]
+            if year == "2018": return ["Czech Republic", "Argentina", "Finland"]
+            if year == "2019": return ["France", "Germany", "China"]
+            if year == "2020": return ["Morocco", "Tunisia", "Poland"]
+
+    def get_has_monthly_data(self, year, country):
+        if self.name == "European Championship":
+            if year == "2021" and (country == "Belgium" or country == "Bulgaria"): return False
+            else: return True
+        else:
+            if year == "2017" and country == "France": return False
+            else: return True
+
+
+    def get_has_yearly_data(self, year, country):
+        if self.name == "European Championship":
+            if year == "2021" and country == "Belgium": return False
+            else: return True
+
+    def get_monthly_data(self, year, country):
+        return pd.DataFrame({
+            "month": ["Jan", "Feb", "Mar", "Apr"],
+            "births": [100, 120, 90, 110]
+            })
+
+    def get_yearly_data(self, year, country):
+        return pd.DataFrame({
+            "years": ["2020", "2021", "2022"],
+            "births": [1200, 1300, 1250]
+            })
+
+
+wc = MockTournament("World Championship")
+ec = MockTournament("European Championship")
 
 tournaments = {"World Championship": wc, "European Championship": ec}
-#makes x an instance of Tournament as in x = tournaments[input.tournament_selection()]
 
-# adjusted size to have both championships on 1 line: 300px or 2lines 200px
 custom_style = ui.tags.style(
     """aside.sidebar {width: 200px !important; min-width: 200px !important;}""")
 
 # UI layout
 app_ui = ui.page_sidebar(
     ui.sidebar(
-        ui.input_radio_buttons("tournament_selection", "Select a tournament:", tournaments ),
-        ui.input_select("available_years_selection"),
-        ui.input_radio_buttons("available_countries_selection"),
+        ui.input_radio_buttons("tournament_selection", "Select a tournament:", list(tournaments.keys())),
+        ui.output_ui("available_years_selection"),
+        ui.output_ui("available_countries_selection"),
         bg="#f8f8f8"
     ),
-    ui.output_ui("selection_summary"),
     output_widget("birth_chart"),
     custom_style
 )
@@ -36,89 +80,60 @@ def server(inputs, outputs, session):
         available_years = tournament.get_available_years()
         return ui.input_select("available_years_selection", "Select year:", available_years)
 
+
+    @render.ui
     def available_countries_selection():
         tournament = tournaments[inputs["tournament_selection"]()]
         available_countries = tournament.get_available_countries(inputs["available_years_selection"]())
         return ui.input_radio_buttons("available_countries_selection", "Select countries:", available_countries)
 
-    def birth_chart_selection():
-        tournament = tournaments[inputs["tournament_selection"]()]
-        has_monthly_data = tournament.get_has_monthly_data(
-            inputs["available_years_selection"], inputs["available_countries_selection"])
-        has_yearly_data = tournament.get_has_yearly_data(
-            inputs["available_years_selection"], inputs["available_countries_selection"])
-
-        if has_monthly_data:
-            monthly_data = tournament.get_monthly_data(inputs["available_years_selection"], inputs["available_countries_selection"])
-            return monthly_chart(monthly_data)
-
-        elif has_yearly_data:
-            yearly_data = tournament.get_yearly_data(inputs["available_years_selection"], inputs["available_countries_selection"])
-            return yearly_chart(yearly_data)
-
-        else: return no_data_chart()
-
 
     @render_widget
-    def monthly_chart(monthly_data):
+    def birth_chart():
+        tournament = tournaments[inputs["tournament_selection"]()]
+        year = inputs["available_years_selection"]()
+        country = inputs["available_countries_selection"]()
 
-        average = monthly_data["births"].mean()
+        if tournament.get_has_monthly_data(year, country):
+            monthly_data = tournament.get_monthly_data(year, country)
+            return draw_chart(monthly_data, "Monthly", "Month", "month")
+
+        elif tournament.get_has_yearly_data(year, country):
+            yearly_data = tournament.get_yearly_data(year, country)
+            return draw_chart(yearly_data, "No monthly data available<br>Yearly", "Year", "years")
+
+        else:
+            return no_data_chart()
+
+    def draw_chart(data, title_prefix, x_title, x_col):
+        average = data["births"].mean()
 
         fig = go.Figure()
         fig.add_trace(go.Scatter(
-            x=monthly_data["month"],
-            y=monthly_data["births"],
+            x=data[x_col],
+            y=data["births"],
             mode="lines+markers",
             name="Births"
         ))
         fig.add_trace(go.Scatter(
-            x=monthly_data["month"],
-            y=[average] * len(monthly_data),
+            x=data[x_col],
+            y=[average] * len(data),
             mode="lines",
             name=f"Average ({int(average)})",
             line=dict(dash="dash", color="red")
         ))
         fig.update_layout(
             title=(
-                f"Monthly Birth Statistics for {inputs['available_countries_selection']()},"
+                f"{title_prefix} Birth Statistics for {inputs['available_countries_selection']()}, "
                 f"following {inputs['tournament_selection']()} {inputs['available_years_selection']()}"
             ),
-            xaxis_title="Month",
-            yaxis_title="Number of Births"
-        )
-        return fig
-
-    @render_widget
-    def yearly_chart(yearly_data):
-        average = yearly_data["births"].mean()
-
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(
-            x=yearly_data["month"],
-            y=yearly_data["births"],
-            mode="lines+markers",
-            name="Births"
-        ))
-        fig.add_trace(go.Scatter(
-            x=yearly_data["month"],
-            y=[average] * len(yearly_data),
-            mode="lines",
-            name=f"Average ({int(average)})",
-            line=dict(dash="dash", color="red")
-        ))
-        fig.update_layout(
-            title=(
-            f"No monthly data available<br>"
-            f"Yearly Birth Statistics for {inputs['available_countries_selection']()},"
-            f"following {inputs['tournament_selection']()} {inputs['available_years_selection']()}"
-            ),
-            xaxis_title="Years",
+            xaxis_title=x_title,
             yaxis_title="Number of Births"
         )
 
         return fig
 
-    @render_widget
+
     def no_data_chart():
         fig = go.Figure()
 
@@ -127,18 +142,18 @@ def server(inputs, outputs, session):
             yaxis=dict(visible=False),
             annotations=[
                 dict(
-                    text=f"No data available for folowing selection:<br>"
+                    text=f"No birth rates available for following selection:<br>"
                          f"{inputs['available_countries_selection']()} at "
                          f"{inputs['tournament_selection']()}, {inputs['available_years_selection']()}",
                     xref="paper",
                     yref="paper",
                     showarrow=False,
                     font=dict(size=20)
-                    )
-                ]
-            )
-
+                )
+            ]
+        )
         return fig
+
 
 app = App(app_ui, server)
 

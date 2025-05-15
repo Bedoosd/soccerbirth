79cb71a526f0651ce00db91efbc3eb7d7a63e642
bedoosd <teck_kenneth@hotmail.com>
2025-05-15 10:34:42 +0200

    tournament methodes voorbereid en shiny mee aangepast

--- Backend/tournament.py
+++ Backend/tournament.py
@@ -1,13 +1,21 @@
+def get_data(query):
+    #even toegevoegd om rode lijn weg te krijgen
+    pass
+
 class Tournament:
     def __init__(self, tournament_name, tournament_year=None):
         self.tournament_name = tournament_name
         self.tournament_year = tournament_year
 
     def get_available_years(self):
-        #code om een lijst met alle jaartallen door te geven voor het EK of WK
-        pass
+        selected_tounament = self.tournament_name
+        query = """ """ #query om juiste jaren binnen te halen (jaar, organiserend land?)
+        df = get_data(query) #get_data stelt de geinporteerde functie (uit backend/database) voor die een df teruggeeft
+        return df
 
     def get_available_countries(self):
-        #tournament_year moet worden ingesteld in de shiny code als attribuut
-        #code om een lijst met landen mee te geven, year wordt meegegeven vanuit shiny
-        pass
+        selected_tournament = self.tournament_name
+        selected_year = self.tournament_year
+        query = """ """ #query om landen binnen te halen  (land, iso, ronde gehaald)
+        df = get_data(query)
+        return df

093a1a96b55a13a32345e5cb9527d2774931e895
bedoosd <teck_kenneth@hotmail.com>
2025-05-15 14:21:04 +0200

    country, tournament and shiny code prepared for data from database. Tested with dummy data

--- Backend/tournament.py
+++ Backend/tournament.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-
+import pandas as pd
 
 def get_data(query):
     #even toegevoegd om rode lijn weg te krijgen
@@ -13,12 +13,14 @@ class Tournament:
         self.target_date = None
 
     def set_tournament_date_and_target(self):
-        tournament_name = self.tournament_name
-        tournament_year = self.tournament_year
-        query = """ """ #Welke datum?
-        date = get_data(query)  # get_data stelt de geinporteerde functie (uit backend/database) voor
-        self.tournament_date = datetime.strptime(date, '%m/%d/%Y')
-        self.target_date = datetime.strptime(date, '%m/%d/%Y') + timedelta(days=266)
+        # tournament_name = self.tournament_name
+        # tournament_year = self.tournament_year
+        # query = """ """ #Welke datum?
+        # date = get_data(query)  # get_data stelt de geinporteerde functie (uit backend/database) voor
+        # self.tournament_date = datetime.strptime(date, '%m/%d/%Y')
+        # self.target_date = datetime.strptime(date, '%m/%d/%Y') + timedelta(days=266)
+        self.tournament_date = datetime.now()
+        self.target_date = datetime.now() + timedelta(days=266)
 
         return
 
@@ -26,23 +28,20 @@ class Tournament:
         # if self.tournament_name == "European Championship":
         #     return ["2021", "2022", "2023"]
         # else: return ["2017","2018", "2019", "2020"]
-        selected_tournament = self.tournament_name
-        query = """ """ #query om juiste jaren binnen te halen (jaar, organiserend land?)
-        df = get_data(query) #get_data stelt de geinporteerde functie (uit backend/database) voor die een df teruggeeft
-        return df
+        # selected_tournament = self.tournament_name
+        # query = """ """ #query om juiste jaren binnen te halen (jaar, organiserend land?)
+        # df = get_data(query) #get_data stelt de geinporteerde functie (uit backend/database) voor die een df teruggeeft
+        return pd.DataFrame({
+    "year": [2000, 2005, 2010, 2015, 2020],
+    "country": ["Belgium", "Netherlands", "Germany", "France", "Italy"]
+})
 
     def get_available_countries(self):
-        # if self.name == "European Championship":
-        #     if year == "2021": return ["Belgium", "Bulgaria", "Croatia"]
-        #     if year == "2022": return ["Czech Republic", "Denmark", "Finland"]
-        #     if year == "2023": return ["France", "Belgium", "Italy"]
-        # else:
-        #     if year == "2017": return ["Japan", "Germany", "France"]
-        #     if year == "2018": return ["Czech Republic", "Argentina", "Finland"]
-        #     if year == "2019": return ["France", "Germany", "China"]
-        #     if year == "2020": return ["Morocco", "Tunisia", "Poland"]
-        selected_tournament = self.tournament_name
-        selected_year = self.tournament_year
-        query = """ """ #query om landen binnen te halen  (land, iso, ronde gehaald)
-        df = get_data(query)
-        return df
+        # selected_tournament = self.tournament_name
+        # selected_year = self.tournament_year
+        # query = """ """ #query om landen binnen te halen  (land, iso, ronde gehaald)
+        # df = get_data(query)
+        return pd.DataFrame({
+    "country": ["Belgium", "Netherlands", "Germany", "France", "Italy", "Spain"],
+    "iso_code": ["BE", "NL", "DE", "FR", "IT", "ES"]
+})

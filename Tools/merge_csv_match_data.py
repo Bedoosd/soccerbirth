0026da8f6ea8f0d8465fd4806a30a723e4ab1d7a
bertje985 <bert_geeroms@hotmail.com>
2025-05-18 13:27:27 +0200

    moved to a function and function call in __main__

--- Tools/merge_csv_match_data.py
+++ Tools/merge_csv_match_data.py
@@ -2,26 +2,22 @@
 from pathlib import Path
 import pandas as pd
 
-csv_files_map = Path.cwd().parent / "data" / "Euro" / "All_matches"
-output_path = csv_files_map.parent / "merged_euro_matches.csv"
-#If output directory doesn't exist --> create
-output_path.parent.mkdir(exist_ok=True)
+def merge_csv_files(input_dir, output_file):
+    if not Path(input_dir).is_dir():
+        raise NotADirectoryError(f"input_directory does not exist: {input_dir}")
+    if not Path(output_file.parent).is_dir():
+        output_path.parent.mkdir(exist_ok=True)
 
-columns_needed = [
-    "id_match", "home_team", "away_team", "home_score", "away_score",
-    "year", "date", "round", "match_attendance"
-]
+if __name__ == "__main__":
+    csv_files_map = Path.cwd().parent / "data" / "Euro" / "All_matches"
+    output_path = csv_files_map.parent / "merged_euro_matches.csv"
 
-valid_csv = []
+    try:
+        merge_csv_files(csv_files_map, output_path)
+    except NotADirectoryError as e:
+        print(e)
+    except FileNotFoundError as e:
+        print(e)
+    except Exception as e:
+        print(f"Unexpected error : {e}")
 
-for f in csv_files_map.glob("*.csv"):
-    if f.stem.isdigit() and len(f.stem) == 4:
-        try:
-            valid_csv.append(pd.read_csv(f, usecols=columns_needed))
-        except ValueError as e:
-            print(f"file skipped (wrong columns): {f.name} – {e}")
-    else:
-        print(f"file skipped (invalid filename): {f.name} – not a 4-digit year")
-
-pd.concat(valid_csv, ignore_index=True).to_csv(output_path, index=False)
-print(f"merged csv saved in: {output_path}")

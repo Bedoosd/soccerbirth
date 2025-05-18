ce972dc1990a8f5ae4470082f45ab5e68a6ae387
bertje985 <bert_geeroms@hotmail.com>
2025-05-18 13:30:40 +0200

    functie compleet gemaakt + output als er geen gevalideerde csv kan gemaakt worden

--- Tools/merge_csv_match_data.py
+++ Tools/merge_csv_match_data.py
@@ -8,6 +8,28 @@ def merge_csv_files(input_dir, output_file):
     if not Path(output_file.parent).is_dir():
         output_path.parent.mkdir(exist_ok=True)
 
+    columns_needed = [
+        "id_match", "home_team", "away_team", "home_score", "away_score",
+        "year", "date", "round", "match_attendance"
+    ]
+
+    valid_csv = []
+
+    for f in csv_files_map.glob("*.csv"):
+        if f.stem.isdigit() and len(f.stem) == 4:
+            try:
+                valid_csv.append(pd.read_csv(f, usecols=columns_needed))
+            except ValueError as e:
+                print(f"file skipped (wrong columns): {f.name} – {e}")
+        else:
+            print(f"file skipped (invalid filename): {f.name} – not a 4-digit year")
+
+    if valid_csv:
+        pd.concat(valid_csv, ignore_index=True).to_csv(output_path, index=False)
+        print(f"merged csv saved in: {output_path}")
+    else:
+        print("No valid CSV files to merge.")
+
 if __name__ == "__main__":
     csv_files_map = Path.cwd().parent / "data" / "Euro" / "All_matches"
     output_path = csv_files_map.parent / "merged_euro_matches.csv"

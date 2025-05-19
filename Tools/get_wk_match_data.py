61847a12872b53031e32f930f0dc4351e53e8f26
bertje985 <bert_geeroms@hotmail.com>
2025-05-19 22:49:40 +0200

    add column winner to new csv file. calculate column winner. copy to new csv wk_match_data.csv

--- Tools/get_wk_match_data.py
+++ Tools/get_wk_match_data.py
@@ -4,3 +4,15 @@ df = pd.read_csv("../Data/World/WK_matches.csv")
 
 cols = ['home_team', 'away_team', 'home_score', 'away_score', 'home_penalty', 'away_penalty', 'Attendance', 'Round', 'Date', 'Host', 'Year']
 df_filtered = df[cols].copy()
+
+def winner(row):
+    if row.home_score != row.away_score:
+        return row.home_team if row.home_score > row.away_score else row.away_team
+    if pd.notna(row.home_penalty) and pd.notna(row.away_penalty):
+        if row.home_penalty != row.away_penalty:
+            return row.home_team if row.home_penalty > row.away_penalty else row.away_team
+    return 'Draw'
+
+df_filtered['Winner'] = df_filtered.apply(winner, axis=1)
+
+df_filtered.to_csv("../Data/World/wk_match_data.csv", index=False)

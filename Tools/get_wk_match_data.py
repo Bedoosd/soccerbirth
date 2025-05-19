c560e23b939db16cee42f34aa9b0d3ce9025197d
bertje985 <bert_geeroms@hotmail.com>
2025-05-19 22:48:11 +0200

    kolommen selecteren copy of dataframe(anders issues)

--- Tools/get_wk_match_data.py
+++ Tools/get_wk_match_data.py
@@ -1,3 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv("../Data/World/WK_matches.csv")
+
+cols = ['home_team', 'away_team', 'home_score', 'away_score', 'home_penalty', 'away_penalty', 'Attendance', 'Round', 'Date', 'Host', 'Year']
+df_filtered = df[cols].copy()
